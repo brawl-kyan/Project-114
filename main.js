54b2830114afefa370ee513ebed467bfc12434b1
brawl-kyan <79627900+brawl-kyan@users.noreply.github.com>
2022-04-01 11:38:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){    
+}
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.position(490,150);    
+}
+
+function draw(){
+}
+
+function snappy(){
+    save('you_are_funny.png') 
+}
